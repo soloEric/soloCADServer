1971c1703ad52274ba02e1dab7e532e542975421
Eric_Larson <eric@gosolo.io>
2020-01-17 08:21:09 -0700

    removed unneeded comments

--- tools/folderManager.js
+++ tools/folderManager.js
@@ -2,17 +2,13 @@ const FS = require('fs');
 const PATH = require('path');
 module.exports = {
 
-    // takes in base folder name, additional name to concat,
-    // and creates the folder in the local directory
     createLocalFolder: (fileName) => {
         FS.mkdirSync(fileName);
     },
 
-    // searches local directory for folder name supplied
-    // deletes all files and folders within
     removeLocalFolder: (fileName) => {
         if (FS.existsSync(fileName)){
-            FS.readdirSync(fileName).forEach((file, index) => {
+            FS.readdirSync(fileName).forEach((file) => {
                 const currentPath = PATH.join(fileName, file);
                 if (FS.lstatSync(currentPath).isDirectory()) {
                     this.removeLocalFolder(currentPath);
@@ -21,6 +17,16 @@ module.exports = {
                 }
             });
             FS.rmdirSync(fileName);
+        }
+    },
+
+    deleteLocalFile: (fileName) => {
+        if (FS.existsSync(fileName)) {
+            FS.unlinkSync(fileName);
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
